b724a6c850c0cee2b1fcf9d65dca69eba56753b0
NahshonYermiyahu <nahshonrm@gmail.com>
2021-05-16 19:44:34 +0300

    16/05-4

--- src/index.js
+++ src/index.js
@@ -1,19 +1,66 @@
 function dropdownUser() {
   document.getElementById("dropdown-user").classList.toggle("show");
-  console.log(document.getElementById("dropdown-user").classList);
 }
 
 window.onclick = function (event) {
   if (!event.target.matches(".header__user-arrow")) {
     let dropdowns = document.getElementsByClassName("dropdown__user-content");
-    console.log(dropdowns);
 
     for (let i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      console.log(openDropdown);
       if (openDropdown.classList.contains("show")) {
         openDropdown.classList.remove("show");
       }
     }
   }
 };
+
+function renderPopup5() {
+  return (location.href = "#popup5");
+}
+
+function renderPopup4() {
+  return (location.href = "#popup4");
+}
+
+function renderPopup3() {
+  return (location.href = "#popup3");
+}
+function renderPopup2() {
+  return (location.href = "#popup2");
+}
+
+function renderPopup1() {
+  return (location.href = "#popup1");
+}
+
+const items = document.getElementsByClassName("item");
+
+for (let item of items) {
+  item.addEventListener("click", () => {
+    item.classList.toggle("selected");
+  });
+}
+
+const theme_btn = document.getElementById("checkbox-theme");
+const body = document.getElementsByTagName("body");
+
+theme_btn.addEventListener("click", () => {
+  for (let item of body) {
+    item.className = theme_btn.checked ? "dark-theme" : "light-theme";
+  }
+});
+
+function sidebarBtn(btn) {
+  const btns = document.getElementsByClassName("sidebar__item-btn");
+  for (let i of btns) {
+    i.classList.remove("activeSidebar");
+  }
+
+  btn.classList.toggle("activeSidebar");
+}
+
+function changeSidebar(btn) {
+  document.getElementById("sidebar").classList.toggle("minSidebar");
+  btn.classList.toggle("changeSidebar");
+}
