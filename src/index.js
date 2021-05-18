3858c6106d7eba198ab6477123cef65eed5101e4
NahshonYermiyahu <nahshonrm@gmail.com>
2021-05-18 18:04:45 +0300

    18/05-3

--- src/index.js
+++ src/index.js
@@ -45,23 +45,56 @@ theme_btn.addEventListener("click", () => {
 
 //#region // ! Выделение строки в sidebar
 function sidebarBtn(btn) {
-  const btns = document.getElementsByClassName("sidebar__item-btn");
-  for (let i of btns) {
-    i.classList.remove("activeSidebar");
+  const btns = document.getElementsByClassName("sidebar__item");
+  const sideBar = document.getElementById("sidebar");
+  if (!sideBar.classList.contains("minSidebar")) {
+    for (let i of btns) {
+      i.classList.remove("activeSidebar");
+      i.classList.remove("activeMinSidebar");
+    }
+    btn.classList.toggle("activeSidebar");
+  } else {
+    for (let i of btns) {
+      i.classList.remove("activeMinSidebar");
+      i.classList.remove("activeSidebar");
+    }
+    btn.classList.toggle("activeMinSidebar");
   }
-  btn.classList.toggle("activeSidebar");
 }
 //#endregion
 
 //#region // !  Изменение размера sidebar
 function changeSidebar(btn) {
   document.getElementById("sidebar").classList.toggle("minSidebar");
+
+  const sidebarItems = document.getElementById("sidebar");
+  const btns2 = document.getElementsByClassName("sidebar__item");
+
+  if (sidebarItems.classList.contains("minSidebar")) {
+    for (let i of btns2) {
+      if (i.classList.contains("activeSidebar")) {
+        i.classList.remove("activeSidebar");
+        i.classList.remove("activeMinSidebar");
+      }
+    }
+  } else {
+    for (let i of btns2) {
+      i.classList.remove("activeMinSidebar");
+      i.classList.remove("activeSidebar");
+    }
+  }
+
   document.getElementById("content-sidebar").classList.toggle("changeSidebar");
   document.getElementById("content").classList.toggle("content-max");
 
-  const btns = document.getElementsByClassName("sidebar__item-btn");
+  const btns = document.getElementsByClassName("sidebar__item-label");
   for (let i of btns) {
     i.classList.toggle("changeBtnSidebar");
+  }
+
+  const items = document.getElementsByClassName("sidebar__item");
+  for (let i of items) {
+    i.classList.toggle("changeItemSidebar");
   }
 }
 //#endregion
